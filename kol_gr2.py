fb5510a3ccfb1356f39125a717577e7c365f0726
Tezeusz  Woronko <4woronko@taurus.fis.agh.edu.pl>
2016-10-12 14:27:44 +0200

    zad01

--- kol_gr2.py
+++ kol_gr2.py
@@ -1,23 +1,30 @@
-#Write a library that contains a class that can represent any 2𝑥2 real matrice. 
-#Include two functions to calculate the sum and product of two matrices. 
-#Next, write a program that imports this library module and use it to perform calculations.
-#Examples:
-#
-# matrix_1 = Matrix(4,5,6,7)
-# matrix_2 = Matrix(2,2,2,1)
-#
-# matrix_3 = matrix_2.add(matrix_1)
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#(If you want you can expand implementation to NxN matrix.)
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#When you are done upload this code to your github repository. 
-#The whole repository MUST be named "kol_gr2"! 
-#
-#Delete these comments before commit!
-#Good luck.
+#!/usr/bin/env python
+
+class Matrix:
+	
+	def __init__(self,elem11,elem12,elem21,elem22):
+		self.__M11 = elem11
+		self.__M12 = elem12
+		self.__M21 = elem21
+		self.__M22 = elem22
+
+	def __str__(self):
+		return str(self.__M11) + ' ' + str(self.__M12) + '\n' + str(self.__M21) + ' ' + str(self.__M22)
+
+	def add(self,matrix2):
+		return Matrix(self.__M11+matrix2.__M11,self.__M12+matrix2.__M12,self.__M21+matrix2.__M21,self.__M22+matrix2.__M22)
+
+	def multiply(self,matrix2):
+		return Matrix(self.__M11*matrix2.__M11 + self.__M12*matrix2.__M21,self.__M11*matrix2.__M12+self.__M12*matrix2.__M22,self.__M21*matrix2.__M11+self.__M22*matrix2.__M21,self.__M21*matrix2.__M12+self.__M22*matrix2.__M22)
+		
+
+
+
+
+m=Matrix(1,2,3,4)
+print m
+n = Matrix(2,3,4,5)
+print
+print m.add(n)
+print
+print m.multiply(n)
